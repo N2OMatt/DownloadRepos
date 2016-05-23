ba686e278691911a8dbacfee208d64f180e4d734
n2omatt <n2o.matt@gmail.com>
2016-05-23 04:26:02 -0300

    Update the script to get the repos automatically.

--- download_n2omatt.py
+++ download_n2omatt.py
@@ -1,34 +1,64 @@
 #!/usr/bin/python
+
+## Imports ##
 import os;
 import os.path;
-import termcolor;
+import urllib;
+import json;
+
+#Sometimes I don't have termcolor.
+try:
+    from termcolor import colored;
+except ImportError, e:
+    def colored(msg, color):
+        return msg;
+
 
 ## Paths.
-GIT_URL      = "git@github.com:N2OMatt/";
-N2OMATT_PATH = "~/Documents/Projects/N2OMatt/";
+BASE_URL  = "https://api.github.com/users/N2OMatt/repos"
+BASE_PATH = os.path.expanduser("~/Documents/Projects/");
 
-# Repositiory - Directory Information.
-REPOS = {
-    "dots"               : N2OMATT_PATH,
-    "DownloadRepos"      : N2OMATT_PATH,
-    "lights-out"         : N2OMATT_PATH,
-    "LinuxTidyAndClean"  : N2OMATT_PATH,
-    "MyCerts"            : N2OMATT_PATH,
-    "n2omatt.github.com" : N2OMATT_PATH,
-    "ProjectEuler"       : N2OMATT_PATH,
-};
+## Helper functions ##
+def make_dir(name):
+    fullname = os.path.join(BASE_PATH, name);
+    if(os.path.isdir(fullname) == False):
+        print "Creating directory: ({0})".format(fullname);
+        os.system("mkdir -p {0}".format(fullname));
 
-# Download the repos...
-for repo_name in sorted(REPOS.keys()):
-    #Build the GIT url and the location of the repo.
-    full_git_url   = os.path.join(GIT_URL, repo_name + ".git");
-    full_repo_path = os.path.expanduser(os.path.join(REPOS[repo_name], repo_name));
+    return fullname;
+
+def fetch_list_repos(organization_name):
+    url      = BASE_URL.format(ORGANIZATION_NAME=organization_name);
+    response = urllib.urlopen(url);
+    data     = json.loads(response.read());
 
 
-    #Already have this repo in this computer.s
-    if(os.path.isdir(full_repo_path)):
-        print "[{}] is already downloaded....".format(termcolor.colored(repo_name, "blue"));
-        continue;
+    print "Fetching repos for: ({0})".format(organization_name);
+    repos = [];
+    for info in data:
+        repos.append(
+            { "url"  : info["clone_url"],
+              "name" : info["name"]
+            });
 
-    os.system("mkdir -p {}".format(full_repo_path));
-    os.system("git clone {} {}".format(full_git_url, full_repo_path));
+    return repos;
+
+def clone_repos(repos_info, repos_dir):
+    for repo_info in repos_info:
+        print "Clonning repo ({0}) in ({1})".format(repo_info["name"], repos_dir);
+
+        repo_full_dir = os.path.join(repos_dir, repo_info["name"]);
+
+        if(os.path.isdir(repo_full_dir)):
+            print "Repo already cloned...";
+            return;
+
+        os.system("git clone {0} {1}".format(repo_info["url"], repo_full_dir));
+
+
+repos_dir  = make_dir("N2OMatt");
+repos_info = fetch_list_repos("N2OMatt");
+clone_repos(repos_info, repos_dir);
+
+print "----"
+
