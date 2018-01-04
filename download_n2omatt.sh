dce8a1c75014efe03615b72adb0d1ca89a9abadb
n2omatt <n2omatt@amazingcow.com>
2018-01-04 16:29:07 -0200

    - Add Not a Bug repos...

--- download_n2omatt.sh
+++ download_n2omatt.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
-##----------------------------------------------------------------------------##
-## File      : download_n2omatt.sh                                            ##
-## Project   : DownloadRepos                                                  ##
-## Author    : n2omatt <n2omatt@amazingcow.com>                               ##
-## Date      : September 06, 2017                                             ##
-## License   : GPLv3                                                          ##
-## Copyright : N2OMatt - 2017, 2018                                           ##
+##~---------------------------------------------------------------------------##
+##                        ____                       _   _                    ##
+##                  _ __ |___ \ ___  _ __ ___   __ _| |_| |_                  ##
+##                 | '_ \  __) / _ \| '_ ` _ \ / _` | __| __|                 ##
+##                 | | | |/ __/ (_) | | | | | | (_| | |_| |_                  ##
+##                 |_| |_|_____\___/|_| |_| |_|\__,_|\__|\__|                 ##
+##                              www.n2omatt.com                               ##
+##  File      : download_n2omatt.sh                                           ##
+##  Project   : DownloadRepos                                                 ##
+##  Date      : Sep 06, 2017                                                  ##
+##  License   : GPLv3                                                         ##
+##  Author    : n2omatt <n2omatt@amaizingcow.com                              ##
+##  Copyright : n2omatt - 2017, 2018                                          ##
 ##                                                                            ##
-## Description:                                                               ##
+##  Description :                                                             ##
 ##   Clone all N2OMatt's public repos.                                        ##
-##----------------------------------------------------------------------------##
+##---------------------------------------------------------------------------~##
 
+
+##----------------------------------------------------------------------------##
+## Github repos                                                               ##
+##----------------------------------------------------------------------------##
 ORGS="N2OMatt";
-BASE_PATH="$HOME/Documents/Projects/";
+BASE_PATH="$HOME/Documents/Projects";
 
 for ORG in $ORGS; do
     ./github_repo_fetcher.py \
@@ -26,3 +36,8 @@ for ORG in $ORGS; do
 
     rm $ORG.list;
 done;
+
+##----------------------------------------------------------------------------##
+## Not a Bug                                                                  ##
+##----------------------------------------------------------------------------##
+git clone https://notabug.org/n2omatt/libreflix $BASE_PATH/$ORGS/libreflix
